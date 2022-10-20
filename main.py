8e9d601c5dffd8392bf7f05cd575b41879e8969d
lama <lamatail@protonmail.com>
2022-10-20 14:21:38 +0300

    change example

--- main.py
+++ main.py
@@ -2,25 +2,27 @@ from example.generator import generate_data, percentile, load_plan
 from metric.metric_table import *
 from db.control import *
 
-if __name__ == '__main__':
+
+def test():
     df = generate_data()
     sla = pd.Series([184] * 5, index=['test1', 'test2', 'test3', 'test4', 'test5'])
-
     trm = TableMetric(df, load_plan, ['name'], time_column='time')
-    trm.metric('delay', 'int32', 'response_time', func=percentile(90))
-    trm.metric('delay', 'int32', 'response_time', func=np.average)
-    # trm.sla(sla, 'sla', lambda x, y: (x - y) / y + 1)
+    trm.metric('delay', 'int32', 'per90', func=percentile(90))
+    #trm.metric('delay', 'int32', 'response_time', func=np.average)
     trm.sla(sla, 'sla')
     trm.assemble_data_metric()
     trm.assemble_data_sla('sla')
-    print(trm.assemble_data().head(10))
-
-    #add_metric(session, name='response_time')
-    #add_operations(['GoPage'])
-    report = add_report(session, name='ih', date=datetime.now(), description='hello')
+    insert_operations(session, [Operation(name=o[0]) for o in trm.operation_name])
+    report = insert_report(session, Report('first', datetime.now()))
     add_report_operation(session, trm, report.id)
-    print(report.id)
     session.close()
-    #print(report.__dict__)
+    print(trm.data_metric_assemble.head())
+    return trm
 
+
+if __name__ == '__main__':
+    #trm = test()
+    report_name = 7
+    print(build_table_metric(session, 7).head())
+    session.close()
 
